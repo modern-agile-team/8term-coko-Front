06d16677c6340e416c6e02d576cfab96fec4bec2
GuDoYoon <rhehfl418q@gmail.com>
2024-10-02 16:14:49 +0900

    🎨 Publish(#32): 그리드 비율 수정

--- src/style/gridSystem.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-export const GridDiv = styled.div`
-  display: grid;
-  margin: 0 24px 0 24px;
-  grid-template-columns: 2fr 6fr 2fr;
-  column-gap: 24px;
-  grid-template-rows: 100vh;
-`;
