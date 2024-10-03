c0eeb5993d3cacc4ead7ee6defab8211ed1507ef
bluetree7878 <bluetree7878@naver.com>
2024-10-03 23:32:30 +0900

    ⚙ Setting(#37): GlobalStyle 및 Maplestory 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './style/GlobalStyle';
+import GlobalFont from './style/GlobalFont';
 import Main from './pages/main/Main';
 import Quest from './pages/Quest/Quest';
 import Ranking from './pages/Ranking/Ranking';
+
 function App() {
   return (
     <>
+      <GlobalStyle />
+      <GlobalFont />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />}></Route>
