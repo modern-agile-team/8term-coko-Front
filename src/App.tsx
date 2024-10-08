b17dc7b739c28919bd90101c54858caa22fb439b
GuDoYoon <rhehfl418q@gmail.com>
2024-10-08 19:13:48 +0900

    🎨 Publish(#34): router 최신화 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Router from './route/router';
+import Router from './route/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 function App() {
--- src/route/Router.tsx
+++ src/route/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../pages/main/Main';
 import Quest from '../pages/Quest/Quest';
 import Ranking from '../pages/Ranking/Ranking';
+import Quiz from '../pages/Quiz/Quiz';
 export default function Router() {
   return (
     <>
@@ -10,6 +11,7 @@ export default function Router() {
           <Route path="/" element={<Main />}></Route>
           <Route path="/quest" element={<Quest />}></Route>
           <Route path="/Ranking" element={<Ranking />}></Route>
+          <Route path="/Quiz/:section/:part" element={<Quiz />}></Route>
         </Routes>
       </BrowserRouter>
     </>
