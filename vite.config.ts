f1d4cb8bb5a2ab5bdd4c939f3c786ebeb09466ba
bluetree7878 <bluetree7878@naver.com>
2024-10-13 16:25:55 +0900

    ⚙ Setting(#47): @swc/plugin-styled-components 적용

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,17 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react({
+      plugins: [
+        [
+          '@swc/plugin-styled-components',
+          {
+            displayName: true,
+            ssr: false,
+          },
+        ],
+      ],
+    }),
+  ],
 });
