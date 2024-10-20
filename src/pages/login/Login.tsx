60c5b62721e0c7132cf798fdc9dbc331a1e2cad9
GuDoYoon <rhehfl418q@gmail.com>
2024-10-20 17:48:31 +0900

    🎨 Publish(#54): 카카오 소셜로그인 링크 생성

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,12 +2,15 @@ import { AlignCenter } from '../../style/LayOut';
 import { FlexContainer, LogoImageSection, SocialLoginLink } from './styles';
 
 export default function Login() {
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${
+    import.meta.env.VITE_KAKAO_REST_API_KEY
+  }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URL}&response_type=code`;
   return (
     <>
       <AlignCenter>
         <FlexContainer>
           <LogoImageSection />
-          <SocialLoginLink to="">카카오</SocialLoginLink>
+          <SocialLoginLink to={kakaoURL}>카카오</SocialLoginLink>
           <SocialLoginLink to="">구글</SocialLoginLink>
           <SocialLoginLink to="">깃허브</SocialLoginLink>
         </FlexContainer>
