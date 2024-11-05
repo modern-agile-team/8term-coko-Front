e8cdc1e7c59aaa34782de25f117f1162ef075766
bluetree7878 <bluetree7878@naver.com>
2024-11-05 19:51:57 +0900

    ⚙ Setting(#37): media로 객체명 변경에 따른 example 수정

--- src/style/media.ts
+++ src/style/media.ts
@@ -1,13 +1,11 @@
 export const media = {
   /**
    * @example
-   * ${({ theme }) => theme.media.desktop} {
+   * ${({ media }) => media.desktop} {
    *   padding: 3px;
    * }
    */
-  media: {
-    desktop: '@media (min-width: 1280px)',
-    tablet: '@media (min-width: 768) and (max-width: 1279px)',
-    mobile: '@media (max-width: 767px)',
-  },
+  desktop: '@media (min-width: 1280px)',
+  tablet: '@media (min-width: 768px) and (max-width: 1279px)',
+  mobile: '@media (max-width: 767px)',
 };
