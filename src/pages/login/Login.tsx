5255b9c5fbed7f77c18adfa862095f662b39ba35
bluetree7878 <bluetree7878@naver.com>
2024-11-19 16:07:18 +0900

    ⚙ Setting(#67): react-cookie 라이브러리 설치 및 cookie 유틸 함수 생성

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -5,6 +5,7 @@ export default function Login() {
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${
     import.meta.env.VITE_KAKAO_REST_API_KEY
   }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URL}&response_type=code`;
+
   return (
     <>
       <AlignCenter>
