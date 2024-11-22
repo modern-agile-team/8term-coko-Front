a9df0e703c67296e2a0aca708f586c99b996c61d
GuDoYoon <rhehfl418q@gmail.com>
2024-11-22 19:54:26 +0900

    🔨 Refactor(#72): 경로 수정, common 절대경로 수정

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,4 +1,4 @@
-import Question from '@/features/quiz/ui/Question';
+import Question from '@features/quiz/ui/Question';
 import { AlignCenter } from '../../style/LayOut';
 import {
   HeaderSection,
@@ -6,24 +6,24 @@ import {
   ResponseButton,
   SubmitSection,
 } from './styles';
-import type Quiz from '@/types/Quiz';
-import { useClientQuizStore } from '@/store/useClientQuizStore';
-import Combination from '@/features/quiz/ui/Combination';
-import MultipleChoice from '@/features/quiz/ui/MultipleChoice';
-import OXSelector from '@/features/quiz/ui/OXSelector';
-import ShortAnswer from '@/features/quiz/ui/ShortAnswer';
-import componentMapping from '@/utils/componentMap';
+import type Quiz from '@type/Quiz';
+import { useClientQuizStore } from '@store/useClientQuizStore';
+import Combination from '@features/quiz/ui/Combination';
+import MultipleChoice from '@features/quiz/ui/MultipleChoice';
+import OXSelector from '@features/quiz/ui/OXSelector';
+import ShortAnswer from '@features/quiz/ui/ShortAnswer';
+import componentMapping from '@utils/componentMap';
 import useBeforeUnload from '@/hooks/useBeforeUnload';
-import Result from '@/features/quiz/ui/Result';
-import TotalResults from '@/features/quiz/ui/TotalResults';
-import isEqualArray from '@/utils/isEqualArray';
-import QuizzesQuery from '@/queries/quizzesQuery';
-import useModal from '@/hooks/useModal';
-import usePreloadImages from '@/hooks/usePreloadImages';
+import Result from '@features/quiz/ui/Result';
+import TotalResults from '@features/quiz/ui/TotalResults';
+import isEqualArray from '@utils/isEqualArray';
+import QuizzesQuery from '@queries/quizzesQuery';
+import useModal from '@hooks/useModal';
+import usePreloadImages from '@hooks/usePreloadImages';
 import { useEffect, useState } from 'react';
-import Header from '@/common/layout/Header';
-import useUserStore from '@/store/useUserStore';
-import ProgressBar from '@/features/progress/ui/ProgressBar';
+import Header from '@common/layout/Header';
+import useUserStore from '@store/useUserStore';
+import ProgressBar from '@features/progress/ui/ProgressBar';
 import { useLocation } from 'react-router-dom';
 
 //퀴즈페이지
