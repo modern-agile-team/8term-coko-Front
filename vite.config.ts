94aaa49ed384b19895a580d4690c29e0989bbfff
bluetree7878 <bluetree7878@naver.com>
2024-11-26 03:13:50 +0900

    ⚙ Setting(#77): vite 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
-
   server: {
     host: '0.0.0.0',
     port: 3000,
