d37014ca3fa3e84c64aa5f1219375dbae23148de
bluetree7878 <bluetree7878@naver.com>
2024-12-12 01:30:03 +0900

    🔨 Refactor(#67): isFirstRender -> isFirstMount 네이밍 변경

--- src/hooks/useIsFirstMount.ts
+++ src/hooks/useIsFirstMount.ts
@@ -1,18 +1,18 @@
 import { useRef, useEffect } from 'react';
 
 /**
- * @description 해당 컴포넌트가 초기 렌더링인지 체크해주는 훅
+ * @description 해당 컴포넌트가 초기 마운트인지 체크해주는 훅
  *
- * @returns {boolean} 초기 렌더링 여부
+ * @returns {boolean} 초기 마운트 여부
  */
 const useIsFirstMount = (): boolean => {
-  const isFirstRender = useRef(true);
+  const isFirstMount = useRef(true);
 
   useEffect(() => {
-    isFirstRender.current = false; // 첫 번째 렌더링 이후 false로 설정
+    isFirstMount.current = false; // 초기 마운트 이후 false로 설정
   }, []);
 
-  return isFirstRender.current;
+  return isFirstMount.current;
 };
 
 export default useIsFirstMount;
