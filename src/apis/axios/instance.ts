e2c586ab572fc807b7f4b0f4e8c18c7ffb93932b
bluetree7878 <bluetree7878@naver.com>
2024-12-24 14:22:50 +0900

    📝 Modify(#79): 인터셉터 instance로 옮기기

--- src/apis/axios/instance.ts
+++ src/apis/axios/instance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import queryString from 'query-string';
+import { getCookie } from '@utils/cookies';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -11,4 +12,29 @@ const api = axios.create({
     });
   },
 });
+
+// 요청 인터셉터
+api.interceptors.request.use(config => {
+  // 쿠키에서 accessToken 가져오기
+  const accessToken: string | undefined = getCookie('accessToken');
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+// 응답 인터셉터
+api.interceptors.response.use(
+  response => {
+    // HTTP 상태 코드가 200번대인 경우
+    console.log(response);
+    return response;
+  },
+  error => {
+    // HTTP 상태 코드가 에러인 경우
+    return Promise.reject(error);
+  }
+);
+
 export default api;
