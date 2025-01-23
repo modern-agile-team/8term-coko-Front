357134257a3bf6b061d2a00212a66ff482d25f1a
bluetree7878 <bluetree7878@naver.com>
2025-01-23 18:21:17 +0900

    🔨 Refactor(#117): mediaQueryMap as const로 변경

--- src/style/mediaQueryMap.ts
+++ src/style/mediaQueryMap.ts
@@ -1,5 +1,5 @@
-export const mediaQueryMap: Record<string, string> = {
+export const mediaQueryMap = {
   mobile: '(max-width: 768px)',
   tablet: '(min-width: 769px) and (max-width: 1279px)',
   desktop: '(min-width: 1280px)',
-};
+} as const;
