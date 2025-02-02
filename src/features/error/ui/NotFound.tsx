dfc1578e25d749c09717d1d7e0077f45d2e2785d
GuDoYoon <rhehfl418q@gmail.com>
2025-02-02 14:57:50 +0900

    🎨 Publish(#124): NotFound 페이지 생성

--- src/features/error/ui/NotFound.tsx
+++ src/features/error/ui/NotFound.tsx
@@ -1,4 +1,10 @@
-import { Link } from 'react-router-dom';
+import {
+  GoToMainLink,
+  NotFoundImage,
+  NotFoundText,
+  NotFoundWrapper,
+} from '@/features/error/ui/styles';
+import { getImageUrl } from '@/utils/getImageUrl';
 
 interface NotFoundProps {
   error?: Error;
@@ -6,11 +12,17 @@ interface NotFoundProps {
 
 export default function NotFound({ error }: NotFoundProps) {
   return (
-    <div>
-      <h1>404</h1>
-      <p>{error?.message}</p>
-      <p>페이지를 찾을 수 없습니다.</p>
-      <Link to="/learn">홈으로 돌아가기</Link>
-    </div>
+    <NotFoundWrapper>
+      <NotFoundText $margin="0 0 74px 0">
+        여기가 어디야? 아무래도 길을 잃어버렸나봐 ! 
+      </NotFoundText>
+      <NotFoundImage src={getImageUrl('404이미지.webp')} />
+      <NotFoundText $margin="45px 0 59px 0">
+        죄송합니다. 페이지를 찾을 수 없습니다. <br />
+        존재하지 않는 주소를 입력하셨거나 요청하신 페이지의 주소가 변경,
+        삭제되어 찾을 수 없습니다.
+      </NotFoundText>
+      <GoToMainLink to="/learn">메인으로</GoToMainLink>
+    </NotFoundWrapper>
   );
 }
--- src/features/error/ui/styles.ts
+++ src/features/error/ui/styles.ts
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const FallbackWrapper = styled.div`
@@ -7,4 +8,37 @@ export const FallbackWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const NotFoundWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NotFoundImage = styled.img`
+  width: 80%;
+  height: 40%;
+`;
+export const NotFoundText = styled.p<{ $margin: string }>`
+  margin: ${({ $margin }) => $margin};
+  text-align: center;
+`;
+export const GoToMainLink = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 213px;
+  height: 47px;
+  border-radius: 15px;
+  background: #0ff;
+  color: #fff;
+  text-align: center;
+  font-size: 23px;
+  line-height: 17px;
+  letter-spacing: 0.16px;
+  box-shadow: 0px 6px #00e4ee;
 `;
