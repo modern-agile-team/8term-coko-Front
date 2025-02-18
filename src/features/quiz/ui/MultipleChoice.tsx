f8c1e7a22009a84b1d8d12083b0a3ab369e94e97
GuDoYoon <rhehfl418q@gmail.com>
2025-02-18 17:22:23 +0900

    ✨ Feature(#85): 객관식 문제 변경

--- src/features/quiz/ui/MultipleChoice.tsx
+++ src/features/quiz/ui/MultipleChoice.tsx
@@ -35,7 +35,7 @@ export default function MultipleChoice({ answerChoice }: MultipleChoiceProps) {
               onClick={() => setUserResponseAnswer(value)}
               $isClick={userResponseAnswer[0] === value}
             >
-              {value}
+              {Number(value) === index + 1 ? value : `${index + 1} : ${value}`}
             </MultipleChoiceButton>
           ))}
         </MultipleChoiceButtonDiv>
