0c15a6c0c3bc82c6d37be0a2a0edba3bd867ba43
bluetree7878 <bluetree7878@naver.com>
2025-02-22 16:35:32 +0900

    ⚙ Setting(#173): Yarn 설정 PnP로 변경 및 Zero-Install

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import mkcert from 'vite-plugin-mkcert';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
-  plugins: [
-    tsconfigPaths(),
-    react({
-      plugins: [
-        [
-          '@swc/plugin-styled-components',
-          {
-            displayName: true,
-            ssr: false,
-          },
-        ],
-      ],
-    }),
-    mkcert(),
-  ],
+  plugins: [tsconfigPaths(), react(), mkcert()],
+  cacheDir: 'vite_cache',
 });
