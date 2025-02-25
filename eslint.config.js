81b2ddecde2bf8536a08ef3a96e353e033661e7d
GuDoYoon <rhehfl418q@gmail.com>
2025-02-25 11:50:38 +0900

    📝 Modify(#170): eslint 설정변경

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
